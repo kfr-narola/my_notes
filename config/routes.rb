5de37fdaaa9680391baa2ec7c83ab24a4ff397e2
kfr-narola <kfr@narola.email>
2020-03-02 18:15:06 +0530

    Feature-1: Integrate admin theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :admins
+  #devise_for :admins
   resources :notes do
     resources :comments
   end
@@ -22,17 +22,11 @@ Rails.application.routes.draw do
   post 'notes/search/', to: "notes#search", as:"search"
 
   root to: 'notes#index'
-  #
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions',
-  #   # registrations: 'users/registrations',
-  #   passwords: 'users/passwords'
-  #
-  # }
+
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
-    registrations: 'admins/registrations',
-    passwords: 'admins/passwords'
+    # registrations: 'admins/registrations',
+    # passwords: 'admins/passwords'
   }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
