dd58cbc363f83ffd0ec9e230f9ff61d25b149c2e
kfr-narola <kfr@narola.email>
2020-03-03 16:24:29 +0530

    Feature-1: Fixed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,32 +2,46 @@ Rails.application.routes.draw do
   #devise_for :admins
   resources :notes do
     resources :comments
+
+    member do
+      get 'mark_as_important/:important', to: "notes#mark_as_important", as: "mark_as_important"
+      get 'share_note'
+      get 'request_edit_permission/:user_id', to: "notes#request_edit_permission", as: "request_edit_permission"
+      get 'assign_edit_permission/:user_id', to: "notes#assign_edit_permission", as: "assign_edit_permission"
+      post 'send_note'
+    end
+
+    collection do
+      get 'autosave/:status', to: "notes#autosave", as: "autosave"
+      post 'search', to: "notes#search", as: "search"
+    end
   end
-  #
+  resources :profile, only: %i[show edit update]
+
   devise_for :users
+  devise_for :admins
 
-  get 'admin/index'
-  get 'admin/manage_users'
-  get 'admin/manage_notes'
-  get 'comments/pagination/:page/:note', to: "comments#pagination", as:"pagination"
-  get 'profile/', to:"profile#index", as:"profile"
-  get 'notes/mark_as_important/:id/:important', to: "notes#mark_as_important", as:"mark_important"
-  get 'notes/autosave/:autosave', to: "notes#autosave", as:"autosave"
-  get 'notes/sharenote/:id', to: "notes#form_share_note", as:"form_share_note"
-  get 'notes/request_note_edit/:id/:user', to: "notes#request_note_edit", as:"request_note_edit"
-  get 'notes/assign_note_edit/:id/:user', to: "notes#assign_note_edit", as:"assign_note_edit"
+  root_to 'notes#index'
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root 'notes#index', as: :authenticated_root
+  #   end
+  #
+  #   unauthenticated do
+  #     root 'devise/sessions#new', as: :unauthenticated_root
+  #   end
+  # end
 
-  post 'notes/sharenote/:id', to: "notes#sharenote", as:"sharenote"
-  patch 'profile/edit/', to:"profile#edit", as:"profile_edit"
-  post 'notes/search/', to: "notes#search", as:"search"
-
-  root to: 'notes#index'
-
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions',
-    # registrations: 'admins/registrations',
-    # passwords: 'admins/passwords'
-  }
+  # devise_scope :admin do
+  #   authenticated :admin do
+  #     root 'admin#index', as: :authenticated_admin_root
+  #   end
+  #
+  #   unauthenticated do
+  #     root 'admin/sessions#new', as: :unauthenticated_admin_root
+  #   end
+  #
+  # end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
