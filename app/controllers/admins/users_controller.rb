94e2e3d1af68c309962fa4b60d74042dc91cea10
kfr-narola <kfr@narola.email>
2020-03-05 11:18:19 +0530

    Feature-3: Make nested controller for admin-panel

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -11,12 +11,17 @@ class Admins::UsersController < AdminController
 
   def update
     @current_ability ||= Ability.new(current_admin)
-    p params[:id]
-    # if @profile.update(profile_paramas)
-    #   respond_to do |format|
-    #     format.js { redirect_to admins_users_path, notice: 'User was successfully updated.' }
-    #   end
-    # end
+    @profile = User.find_by(id:params[:id]).profile
+    p @profile.username
+    if @profile.update(profile_paramas)
+      respond_to do |format|
+        format.js { redirect_to admins_users_path, notice: 'User was successfully updated.' }
+      end
+    end
+  end
+
+  def destroy
+    
   end
 
   private
